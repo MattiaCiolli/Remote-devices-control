fc438ca317f31d231a37510c014b0e9049da0b35
Mattia <mattia93c@gmail.com>
2017-11-02 13:13:54 +0100

    tcp packets to remote device

--- Tesi/asynchronousserv/Classes/IPDevice.cs
+++ Tesi/asynchronousserv/Classes/IPDevice.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -50,9 +51,47 @@ namespace asynchronousserv
             return "ok,ok,ok";
         }
 
-        public string CheckVoltage(string PhoneNumber_in)
+        public string CheckVoltage(string IpAddress_in)
         {
+            TcpClient client = new TcpClient();
+            client.Connect(IpAddress_in, 49152);
+            NetworkStream tcpStream = client.GetStream();
+            byte[] myWriteBuffer = CreateMessage(175, 63, 33, 141, 10); //"af 3f 21 8d 0a";
+            tcpStream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
+
+            try
+            {
+                byte[] myReadBuffer = new byte[1024];
+                StringBuilder myCompleteMessage = new StringBuilder();
+                int numberOfBytesRead = 0;
+
+                // Incoming message may be larger than the buffer size.
+                do
+                {
+                    numberOfBytesRead = tcpStream.Read(myReadBuffer, 0, myReadBuffer.Length);
+
+                    myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+
+                }
+                while (tcpStream.DataAvailable);
+
+                // Print out the received message to the console.
+                Console.WriteLine("You received the following message : " +
+                                             myCompleteMessage);
+            }
+            catch
+            {
+                Console.WriteLine("Sorry.  You cannot read from this NetworkStream.");
+            }
             return "10,10,10";
+        }
+
+        public byte[] CreateMessage(params int[] parameters)
+        {
+            var buf = new byte[parameters.Length];
+            for (int i = 0; i < parameters.Length; i++)
+                Array.Copy(BitConverter.GetBytes(parameters[i]), 0, buf, i, 1);
+            return buf;
         }
     }
 }
