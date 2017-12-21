fa2d8c8def85595066b4de9d2bd0b46cce164fcb
Mattia <mattia93c@gmail.com>
2017-12-21 16:28:37 +0100

    menu e fix grafici

--- ZeccaWebApplication/ZeccaWebSite_App/src/pages/home/home.ts
+++ ZeccaWebApplication/ZeccaWebSite_App/src/pages/home/home.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, MenuController, App } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { DevSel } from '../devSelector/devSelector';
+
+@Component({
+    template: `
+<ion-header>
+    <ion-navbar>
+        <button ion-button menuToggle icon-only>
+            <ion-icon name='menu'></ion-icon>
+        </button>
+        <ion-title>
+            <ion-grid>
+                <ion-row>
+                    <ion-col col-4 offset-4>
+                        <img src="img/zecca.png" id="logo" />
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+        </ion-title>
+    </ion-navbar>
+</ion-header>
+`
+})
+export class BasicPage {
+    constructor(app: App, menu: MenuController) {
+        menu.enable(true);
+    }
+
+    openAddPage() {
+        //this.nav.push(AddPage)
+    }
+}
 
 @Component({
     selector: 'page-home',
