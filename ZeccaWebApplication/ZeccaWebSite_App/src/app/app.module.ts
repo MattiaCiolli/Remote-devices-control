79cf90257f476f0463aa44019a3c391466cff8a5
Mattia <mattia93c@gmail.com>
2018-02-06 18:28:32 +0100

    popover

--- ZeccaWebApplication/ZeccaWebSite_App/src/app/app.module.ts
+++ ZeccaWebApplication/ZeccaWebSite_App/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { DevSel } from '../pages/devSelector/devSelector';
+import { PopoverPage } from '../pages/devSelector/devSelector';
 import { LoginPage } from '../pages/login/login';
 
 @NgModule({
@@ -11,7 +12,8 @@ import { LoginPage } from '../pages/login/login';
     MyApp,
       HomePage,
       DevSel,
-      LoginPage
+      LoginPage,
+      PopoverPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -21,7 +23,8 @@ import { LoginPage } from '../pages/login/login';
     MyApp,
       HomePage,
       DevSel,
-      LoginPage
+      LoginPage,
+      PopoverPage
   ],
   providers: [
       { provide: ErrorHandler, useClass: IonicErrorHandler },
--- ZeccaWebApplication/ZeccaWebSite_App/src/pages/devSelector/devSelector.ts
+++ ZeccaWebApplication/ZeccaWebSite_App/src/pages/devSelector/devSelector.ts
@@ -1,6 +1,109 @@
-﻿import { Component } from '@angular/core';
-import { NavController, NavParams} from 'ionic-angular';
+﻿import { Component, ViewChild, ElementRef  } from '@angular/core';
+import { NavController, NavParams, PopoverController, ViewController } from 'ionic-angular';
 import { Http } from '@angular/http';
+
+
+@Component({
+    template: `
+<ion-searchbar [(ngModel)]="searchterm" (ionInput)="setFilteredItems()"></ion-searchbar>
+<ion-list radio-group [(ngModel)]="selected">
+<ion-item *ngFor="let dev of devs">
+<ion-label>{{dev.id}}</ion-label>
+<ion-radio (ionSelect)="close(dev.id)"></ion-radio>
+</ion-item>
+</ion-list>
+`
+})
+
+export class PopoverPage {
+    searchterm: string = '';
+    selected: string;
+    background: string;
+    //contentEle: any;
+    //textEle: any;
+    devsTot: any[];
+    devs: any[];
+    fontFamily;
+
+    colors = {
+        'white': {
+            'bg': 'rgb(255, 255, 255)',
+            'fg': 'rgb(0, 0, 0)'
+        },
+        'tan': {
+            'bg': 'rgb(249, 241, 228)',
+            'fg': 'rgb(0, 0, 0)'
+        },
+        'grey': {
+            'bg': 'rgb(76, 75, 80)',
+            'fg': 'rgb(255, 255, 255)'
+        },
+        'black': {
+            'bg': 'rgb(0, 0, 0)',
+            'fg': 'rgb(255, 255, 255)'
+        },
+    };
+
+    constructor(private navParams: NavParams, public viewCtrl: ViewController) {
+    }
+
+    setFilteredItems() {
+
+        this.devs = this.devsTot.filter((dev) => {
+            return dev.id.toLowerCase().indexOf(this.searchterm.toLowerCase()) > -1;
+        });    
+
+    }
+
+    close(selected) {
+        this.viewCtrl.dismiss(selected);
+    }
+
+    ngOnInit() {
+        if (this.navParams.data) {
+            //this.contentEle = this.navParams.data.contentEle;
+            //this.textEle = this.navParams.data.textEle;
+            this.devsTot = this.navParams.data.devsTot;
+            this.devs = this.devsTot;
+            //this.background = this.getColorName(this.contentEle.style.backgroundColor);
+            //this.setFontFamily();
+        }
+    }
+
+    /*getColorName(background) {
+        let colorName = 'white';
+
+        if (!background) return 'white';
+
+        for (var key in this.colors) {
+            if (this.colors[key].bg == background) {
+                colorName = key;
+            }
+        }
+
+        return colorName;
+    }
+    
+    setFontFamily() {
+        if (this.textEle.style.fontFamily) {
+            this.fontFamily = this.textEle.style.fontFamily.replace(/'/g, "");
+        }
+    }
+    
+    changeBackground(color) {
+        this.background = color;
+        this.contentEle.style.backgroundColor = this.colors[color].bg;
+        this.textEle.style.color = this.colors[color].fg;
+    }
+
+    changeFontSize(direction) {
+        this.textEle.style.fontSize = direction;
+    }
+
+    changeFontFamily() {
+        if (this.fontFamily) this.textEle.style.fontFamily = this.fontFamily;
+    }*/
+}
 
 @Component({
     selector: 'page-devSelector',
@@ -9,12 +112,14 @@ import { Http } from '@angular/http';
 
 export class DevSel {
 
+    @ViewChild('popoverContent', { read: ElementRef }) content: ElementRef;
+    @ViewChild('popoverText', { read: ElementRef }) text: ElementRef;
     public loading = false;
     public devices: any[];
     public functions: any[];
     public requestResults: any[];
 
-    selectedDevice: string;
+    selectedDevice: string = "--- ";
     selectedFunctions: number[];
 
     deviceFunctions(selectedDev) {
@@ -34,7 +139,18 @@ export class DevSel {
             });
     }
 
-    constructor(private http: Http) {
+    presentPopover(ev?: Event) {
+
+        let popover = this.popoverCtrl.create(PopoverPage, {
+            //contentEle: this.content.nativeElement,
+            //textEle: this.text.nativeElement,
+            devsTot: this.devices
+        });
+        popover.present({ ev: ev });
+        popover.onDidDismiss(data => { this.selectedDevice = data; this.deviceFunctions(this.selectedDevice);});
+    }
+
+    constructor(private http: Http, private popoverCtrl: PopoverController) {
         this.http.get('http://localhost:54610/Devices')
             .subscribe(res => this.devices = res.json());
     }
